3032282b1d6e679703cf93c9735448d589f9c85e
r3dDoX <r3dDoX@users.noreply.github.com>
2021-09-13 17:42:10 +0200

    render blob tokens

--- contracts/Blob.sol
+++ contracts/Blob.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "base64-sol/base64.sol";
 
 contract Blob is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
@@ -21,7 +23,14 @@ contract Blob is ERC721, ERC721Enumerable, Ownable {
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        return "TO_BE_IMPLEMENTED";
+        string[3] memory parts;
+        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"> <rect width="100%" height="100%" fill="palevioletred"></rect> <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="papayawhip" font-size="200px">';
+        parts[1] = Strings.toString(tokenId);
+        parts[2] = '</text></svg>';
+
+        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2]));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "BLOB #', Strings.toString(tokenId), '", "description": "Enjoy your personal token!", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        return string(abi.encodePacked('data:application/json;base64,', json));
     }
 
     // The following functions are overrides required by Solidity.
--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import './App.css';
 import {useState} from 'react';
 import {ethers} from 'ethers';
-import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import Blob from './artifacts/contracts/Blob.sol/Blob.json';
-import version from './blob-contract-address.json'
-
-const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-const blobAddress = version;
+import blobAddress from './blob-contract-address.json';
 
 function App() {
-  const [greeting, setGreetingValue] = useState();
+  const [blobs, setBlobs] = useState([]);
 
   async function requestAccount() {
     await window.ethereum.request({method: 'eth_requestAccounts'});
-  }
-
-  async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
-      try {
-        const data = await contract.greet();
-        console.log('data: ', data);
-      } catch (err) {
-        console.log('Error: ', err);
-      }
-    }
-  }
-
-  // call the smart contract, send an update
-  async function setGreeting() {
-    if (!greeting) return;
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
-      const transaction = await contract.setGreeting(greeting);
-      await transaction.wait();
-      fetchGreeting();
-    }
   }
 
   async function mintBlob() {
@@ -67,20 +36,19 @@ function App() {
 
     for (let i = 0; i < balance; i++) {
       const token = await contract.tokenOfOwnerByIndex(address, i);
-      console.log("token URI:", await contract.tokenURI(token));
+      const tokenUri = await contract.tokenURI(token);
+      const decodedToken = JSON.parse(atob(tokenUri.split(',')[1]));
+      setBlobs((prevBlobs) => [...prevBlobs, decodedToken]);
     }
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={mintBlob}>Mint Blob</button>
-        <button onClick={fetchBlobs}>Fetch Blobs</button>
-        <br />
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
-      </header>
+      <button onClick={mintBlob}>Mint Blob</button>
+      <button onClick={fetchBlobs}>Fetch Blobs</button>
+      <div className="blobs">
+        {blobs.map((blob, index) => <img key={index} src={blob.image} alt={blob.name}/>)}
+      </div>
     </div>
   );
 }
